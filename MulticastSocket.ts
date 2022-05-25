13189d7182c9d434bca23980e40da301ddb5393b
Filipe Oliveira <ooliveirafilipe@gmail.com>
2022-05-25 15:43:00 -0300

    refactor: remove namespace left only class

--- MulticastSocket.ts
+++ MulticastSocket.ts
@@ -1,48 +1,47 @@
 import dgram, { RemoteInfo, Socket } from "node:dgram";
 
-export namespace MulticastSocketCreator {
-  export const start = async (port: number) => {
-    const socket = dgram.createSocket({
+export type Message = { message: Buffer; rinfo: RemoteInfo };
+
+export class MulticastSocket {
+  #socket: Socket;
+  #multicastAddr: string;
+  #multicastPort: number;
+  #receivePromise: any;
+  #messageListener: any;
+
+  constructor() {}
+
+  start(multicastPort: number) {
+    if (this.#socket) return Promise.resolve();
+    this.#socket = dgram.createSocket({
       type: "udp4",
       reuseAddr: true,
     });
 
-    return new Promise<MSInstance>((res, rej) => {
-      socket.on("listening", () => res(new MSInstance(socket, port)));
-      socket.bind(port, () => {});
-    });
-  };
-}
-
-class MSInstance {
-  socket: Socket;
-  multicastAddr: string;
-  multicastPort: number;
-  receivePromise: any;
-  messageListener: any;
-
-  constructor(socket: Socket, multicastPort: number) {
-    this.socket = socket;
-    this.multicastPort = multicastPort;
-    const thisRef = this;
-    this.socket.on("message", (message: Buffer, rinfo: RemoteInfo) =>
-      this._listener({ message, rinfo }, thisRef)
+    this.#multicastPort = multicastPort;
+    this.#socket.on("message", (message: Buffer, rinfo: RemoteInfo) =>
+      this.#listener({ message, rinfo }, this)
     );
+
+    return new Promise<void>((res, rej) => {
+      this.#socket.on("listening", () => res());
+      this.#socket.bind(this.#multicastPort, () => {});
+    });
   }
 
   join(multicastAddr: string) {
-    this.multicastAddr = multicastAddr;
-    this.socket.addMembership(multicastAddr);
+    this.#multicastAddr = multicastAddr;
+    this.#socket.addMembership(multicastAddr);
   }
 
   async send(
     message: string,
-    address = this.multicastAddr,
-    port = this.multicastPort
+    address = this.#multicastAddr,
+    port = this.#multicastPort
   ) {
     return new Promise((res, rej) => {
       const bufferedMessage = Buffer.from(message, "utf-8");
-      this.socket.send(
+      this.#socket.send(
         bufferedMessage,
         0,
         bufferedMessage.length,
@@ -54,7 +53,7 @@ class MSInstance {
   }
 
   onMessage(listener: (data: Message) => void) {
-    this.messageListener = listener;
+    this.#messageListener = listener;
   }
 
   async receive(): Promise<{
@@ -62,19 +61,17 @@ class MSInstance {
     rinfo: { address: string; port: number };
   }> {
     return new Promise((res, rej) => {
-      this.receivePromise = res;
+      this.#receivePromise = res;
     });
   }
 
-  _listener(data: Message, thisRef: MSInstance) {
-    if (thisRef.messageListener) {
-      thisRef.messageListener(data);
+  #listener(data: Message, thisRef: MulticastSocket) {
+    if (thisRef.#messageListener) {
+      thisRef.#messageListener(data);
     }
-    if (thisRef.receivePromise) {
-      thisRef.receivePromise(data);
-      thisRef.receivePromise = undefined;
+    if (thisRef.#receivePromise) {
+      thisRef.#receivePromise(data);
+      thisRef.#receivePromise = undefined;
     }
   }
 }
-
-export type Message = { message: Buffer; rinfo: RemoteInfo };
